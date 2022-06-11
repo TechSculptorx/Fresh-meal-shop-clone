d8501a8a8ade8ad637f1d9e36ec67b5676ce8bce
Priyanshu2055 <priyanshuvanra2@gmail.com>
2022-06-11 19:37:30 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  $(".food-slider").slick({
+    autoplay: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: ".prev-btn",
+    nextArrow: ".next-btn",
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  $(".nav-trigger").click(function () {
+    $(".site-content-wrapper").toggleClass("scaled");
+  });
+});
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+});
